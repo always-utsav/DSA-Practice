9f114a69e38c674371f3a21dfe91d7bfa68e0658
Utsav Kumawat <utsavkumawat05@gmail.com>
2025-08-07 23:21:02 +0530

    Update 09_PALINDROME.cpp

--- DAY-3/09_PALINDROME.cpp
+++ DAY-3/09_PALINDROME.cpp
@@ -5,4 +5,14 @@ public:
         long long rem;
         long long newnum=0;
         if(x<0)
-           
+            return false;
+        else{
+            while(x>0){
+                rem=x%10;
+                newnum=(newnum*10) + rem;
+                x /= 10;
+            }
+               
+        }
+    }
+};
